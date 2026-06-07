300173dae6a272ea4ef5ef234ecf58d38c7b7c92
sammar abbass <sammarabbass416@gmail.com>
2026-06-07 20:47:58 +0500

    protiofilo commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     document.querySelector(this.getAttribute('href'))
+//       .scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+
+
+  document.getElementById("downloadBtn").addEventListener("click", function() {
+    const link = document.createElement("a");
+    link.href = "cv.pdf"; // path to your PDF
+    link.download = "SummerPlays_CV.pdf"; // name for download
+    link.click();
+<<<<<<< HEAD
+  });
+=======
+  });
+>>>>>>> aed3615 (protofilo commit)
